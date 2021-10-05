9342b988aa453e9df889cc902b7c6ff6b099e941
caglarcercinli <59973993+caglarcercinli@users.noreply.github.com>
2021-10-05 21:40:22 +0200

    eerste commit

---
+++ src/test/java/com/udemycrudproject/UdemyCruDprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCruDprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
